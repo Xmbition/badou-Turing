1a12d0a3c792e4607dd9d8e4320f1e8d5d621c6a
21-夏相-福建 <1113792657@qq.com>
2021-07-03 12:29:45 +0800

    第二次作业提交

---
+++ 21-夏相-福建/第二周/code.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+def function(img):
+    height,width,channels=img.shape
+    emptylmage=np.zeros((800,800,channels),np.unit8)
+    sh=800/height
+    sw=800/width
+    for i in range (800):
+        for j in range(800):
+            x=int(i/sh)
+            y=int(j/sw)
+            emptylmage[i,j]=img[x,y]
+    return emptylmage
+
+img=cv2.imread("lenna.png")
+zoom=function(img)
+print(zoom)
+print(zoom.shape)
+cv2.imshow("nearest sharp",zoom)
+cv2.imshow("image",img)
+cv2.waitKey(0)
